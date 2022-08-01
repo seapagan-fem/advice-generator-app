21a64840fcc5a729594c2ce81f302dd540189f6c
Grant Ramsay <seapagan@gmail.com>
2022-08-01 09:21:20 +0100

    get advice from API

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,20 @@
 console.log("JavaScript loaded.")
+
+// get a nugget of advice from the API
+const getAdvice = async () => {
+  const data = await fetch("https://api.adviceslip.com/advice",{cache: 'no-store'})
+    .then(res => res.json())
+  const { id, advice } = data.slip
+
+  document.getElementById("advice-id").innerText = id
+  document.getElementById("advice").innerText = `“${advice}”`
+  }
+
+// trigger on the button
+document.getElementById("advice-button").addEventListener("click", () => {
+  getAdvice()
+})
+
+
+// get a new advice on load/reload
+getAdvice()
